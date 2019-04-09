2b09af0551231929cf180b59ec4207639f684751
DavidCalderon1 <dlcalderonc@gmail.com>
2019-04-08 19:35:39 -0500

    cambios en migraciones

--- database/migrations/2019_04_06_223547_create_orders_table.php
+++ database/migrations/2019_04_06_223547_create_orders_table.php
@@ -21,9 +21,9 @@ class CreateOrdersTable extends Migration {
 			$table->integer('storage_id')->index('fk_orders_storages1_idx');
 			$table->integer('users_addresses_id')->nullable()->index('fk_orders_users_addresses1_idx');
 			$table->dateTime('delivery_date')->nullable();
-			$table->integer('priority',1)->nullable();
+			$table->integer('priority')->nullable();
 			$table->integer('status_id')->nullable()->index('fk_orders_status1_idx');
-			$table->integer('paid',1)->default(0);
+			$table->integer('paid')->default(0);
 			$table->timestamps();
 			$table->softDeletes();
 		});
